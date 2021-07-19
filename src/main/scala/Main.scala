3c92e4f3263cc5bc9ba501dc0b89afd1e0bdcc4a
Karl N <khoa.norden@gmail.com>
2021-07-19 12:17:53 -0700

    updated

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,13 +4,23 @@
 
 //def msg = "I was compiled by Scala 3. :)"
 import java.util.Scanner
+import scala.util.Random
 
 @main def printInts: Unit =
   print("\nEnter an upper bound to iterate: ")
+
+  // don't use this,
+  // because scala.StdIn.readInt doesn't know how to deal with space (\s)
   //Range(0, StdIn.readLine.toInt).map(x => println(x))
+
   val upperBound = {
     val scanner = new Scanner(System.in)
     scanner.nextInt()
   }
-  Range(0, upperBound).map(x => println(x + 1))
+  val rnd = new Random
+  val list = (1 to upperBound)
+                .toList
+                .map(_ * 0 + (rnd.nextGaussian * 10).toInt % 10)
+
+  list.map(x => println(x))
 
